f8d3259296a41c78902a8191e9c19696fd046c9a
Timm Friebe <thekid@thekid.de>
2013-04-06 14:05:56 +0200

    - Test replacing multiple variables

--- src/test/php/com/github/mustache/RenderingTest.class.php
+++ src/test/php/com/github/mustache/RenderingTest.class.php
@@ -15,5 +15,13 @@
         create(new MustacheEngine())->render('Hello {{name}}', array('name' => 'World'))
       );
     }
+
+    #[@test]
+    public function replace_two_variables() {
+      $this->assertEquals(
+        'Hello Chris, You have just won $10000!',
+        create(new MustacheEngine())->render('Hello {{name}}, You have just won ${{value}}!', array('name' => 'Chris', 'value' => 10000))
+      );
+    }
   }
 ?>
