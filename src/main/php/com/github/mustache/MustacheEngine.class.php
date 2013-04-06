e2b674fa37941fa8308732849c2e06e9f5b2cfe8
Timm Friebe <thekid@thekid.de>
2013-04-06 17:25:18 +0200

    - Remove unused method

--- src/main/php/com/github/mustache/MustacheEngine.class.php
+++ src/main/php/com/github/mustache/MustacheEngine.class.php
@@ -6,11 +6,6 @@
    */
   class MustacheEngine extends \lang\Object {
 
-    protected static function variable($name, $default= '""') {
-      $v= '$values[\''.$name.'\']';
-      return 'isset('.$v.') ? '.$v.' : '.$default;
-    }
-
     public function render($template, $values) {
       return Node::parse($template)->evaluate($values);
     }
