a11e870094000e6dd44c18becd6a03c27a5228ae
Timm Friebe <thekid@thekid.de>
2013-04-06 14:04:17 +0200

    - Initial release

---
+++ src/main/php/com/github/mustache/MustacheEngine.class.php
@@ -0,0 +1,10 @@
+<?php
+  namespace com\github\mustache;
+
+  /**
+   * @see  http://mustache.github.io/mustache.5.html
+   */
+  class MustacheEngine extends \lang\Object {
+
+  }
+?>
---
+++ src/test/php/com/github/mustache/RenderingTest.class.php
@@ -0,0 +1,11 @@
+<?php
+  namespace com\github\mustache;
+
+  class RenderingTest extends \unittest\TestCase {
+
+    #[@test]
+    public function new_engine() {
+      new MustacheEngine();
+    }
+  }
+?>
