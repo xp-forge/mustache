bcc5c911b91e39aeef7ef026700f2bcbe87ce1fc
Timm Friebe <thekid@thekid.de>
2013-04-06 14:04:54 +0200

    - Implement first working version

--- src/main/php/com/github/mustache/MustacheEngine.class.php
+++ src/main/php/com/github/mustache/MustacheEngine.class.php
@@ -6,5 +6,12 @@
    */
   class MustacheEngine extends \lang\Object {
 
+    public function render($template, $values) {
+      return preg_replace_callback(
+        '/\{\{([^\}]+)\}\}/',
+        function($variable) use($values) { return $values[$variable[1]]; },
+        $template
+      );
+    }
   }
 ?>
--- src/test/php/com/github/mustache/RenderingTest.class.php
+++ src/test/php/com/github/mustache/RenderingTest.class.php
@@ -7,5 +7,13 @@
     public function new_engine() {
       new MustacheEngine();
     }
+
+    #[@test]
+    public function replace_single_variable() {
+      $this->assertEquals(
+        'Hello World',
+        create(new MustacheEngine())->render('Hello {{name}}', array('name' => 'World'))
+      );
+    }
   }
 ?>
