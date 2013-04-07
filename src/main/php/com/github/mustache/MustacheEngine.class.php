59471cf1cc44642a0e96888ff0a4a6aec91ee076
Timm Friebe <thekid@thekid.de>
2013-04-07 12:21:09 +0200

    - Move parsing back to engine # Preparing partials, part 2

--- src/main/php/com/github/mustache/MustacheEngine.class.php
+++ src/main/php/com/github/mustache/MustacheEngine.class.php
@@ -16,6 +16,48 @@
       return $this;
     }
 
+    public function parse($template) {
+      $parsed= new NodeList();
+      $parents= array();
+      $st= new \text\StringTokenizer($template, '{');
+      while ($st->hasMoreTokens()) {
+
+        // Text
+        if ('' !== ($text= $st->nextToken())) {
+          $parsed->add(new TextNode($text));
+        }
+        if (!$st->hasMoreTokens()) break;
+
+        // Found a tag
+        $st->nextToken('{');
+        $tag= $st->nextToken('}');
+        $st->nextToken('}');
+
+        if ('#' === $tag{0} || '^' === $tag{0}) {  // start section
+          $name= substr($tag, 1);
+          $parents[$name]= $parsed;
+          $parsed= $parsed->add(new SectionNode($name, '^' === $tag{0}));
+          $st->nextToken("\n");
+        } else if ('/' === $tag{0}) {              // end section
+          $name= substr($tag, 1);
+          $parsed= $parents[$name];
+          $st->nextToken("\n");
+        } else if ('&' === $tag{0}) {              // & for unescaped
+          $parsed->add(new VariableNode(ltrim(substr($tag, 1), ' '), FALSE));
+        } else if ('{' === $tag{0}) {              // triple mustache for unescaped
+          $parsed->add(new VariableNode(substr($tag, 1), FALSE));
+          $st->nextToken('}');
+        } else if ('!' === $tag{0}) {              // ! ... for comments
+          $parsed->add(new CommentNode(ltrim(substr($tag, 1), ' '), FALSE));
+        } else {
+          $parsed->add(new VariableNode($tag));
+        }
+      }
+
+      // \util\cmd\Console::writeLine($parsed);
+      return $parsed;
+    }
+
     public function render($template, $arg) {
       if ($arg instanceof Context) {
         $context= $arg;
@@ -24,7 +66,7 @@
         $context->variables= $arg;
         $context->engine= $this;
       }
-      return Node::parse($template)->evaluate($context);
+      return $this->parse($template)->evaluate($context);
     }
 
     public function transform($name, $arg) {
--- src/main/php/com/github/mustache/Node.class.php
+++ src/main/php/com/github/mustache/Node.class.php
@@ -4,47 +4,5 @@
   abstract class Node extends \lang\Object {
 
     public abstract function evaluate($context);
-
-    public static function parse($template) {
-      $parsed= new NodeList();
-      $parents= array();
-      $st= new \text\StringTokenizer($template, '{');
-      while ($st->hasMoreTokens()) {
-
-        // Text
-        if ('' !== ($text= $st->nextToken())) {
-          $parsed->add(new TextNode($text));
-        }
-        if (!$st->hasMoreTokens()) break;
-
-        // Found a tag
-        $st->nextToken('{');
-        $tag= $st->nextToken('}');
-        $st->nextToken('}');
-
-        if ('#' === $tag{0} || '^' === $tag{0}) {  // start section
-          $name= substr($tag, 1);
-          $parents[$name]= $parsed;
-          $parsed= $parsed->add(new SectionNode($name, '^' === $tag{0}));
-          $st->nextToken("\n");
-        } else if ('/' === $tag{0}) {              // end section
-          $name= substr($tag, 1);
-          $parsed= $parents[$name];
-          $st->nextToken("\n");
-        } else if ('&' === $tag{0}) {              // & for unescaped
-          $parsed->add(new VariableNode(ltrim(substr($tag, 1), ' '), FALSE));
-        } else if ('{' === $tag{0}) {              // triple mustache for unescaped
-          $parsed->add(new VariableNode(substr($tag, 1), FALSE));
-          $st->nextToken('}');
-        } else if ('!' === $tag{0}) {              // ! ... for comments
-          $parsed->add(new CommentNode(ltrim(substr($tag, 1), ' '), FALSE));
-        } else {
-          $parsed->add(new VariableNode($tag));
-        }
-      }
-
-      // \util\cmd\Console::writeLine($parsed);
-      return $parsed;
-    }
   }
 ?>
