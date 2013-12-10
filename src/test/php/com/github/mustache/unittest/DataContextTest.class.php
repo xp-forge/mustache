b1c8f63d3f80263a63e810d40c2edbad5eff999b
Timm Friebe <thekid@thekid.de>
2013-12-10 14:39:09 +0100

    Add tests for helper lookup

--- src/test/php/com/github/mustache/unittest/DataContextTest.class.php
+++ src/test/php/com/github/mustache/unittest/DataContextTest.class.php
@@ -9,10 +9,11 @@ class DataContextTest extends \unittest\TestCase {
    * Creates new fixture
    *
    * @param  [:string] $variables
+   * @param  [:var] $helpers
    * @return com.github.mustache.Context
    */
-  public function newFixture($variables) {
-    return new DataContext($variables, new MustacheEngine());
+  public function newFixture($variables, $helpers= array()) {
+    return new DataContext($variables, create(new MustacheEngine())->withHelpers($helpers));
   }
 
   #[@test]
@@ -119,5 +120,22 @@ class DataContextTest extends \unittest\TestCase {
       public function offsetUnset($h) { /* Empty */ }
     }'));
     $this->assertEquals('data', $fixture->lookup('test'));
+  }
+
+  #[@test]
+  public function using_helper() {
+    $fixture= $this->newFixture(array(), array(
+      'test' => function($node, $ctx) { return 'data'; }
+    ));
+    $this->assertInstanceOf('Closure', $fixture->lookup('test'));
+  }
+
+  #[@test]
+  public function variables_are_preferred_over_helpers() {
+    $fixture= $this->newFixture(
+      array('test' => 'variable'),
+      array('test' => function($node, $ctx) { return 'helper'; })
+    );
+    $this->assertEquals('variable', $fixture->lookup('test'));
   }
 }
