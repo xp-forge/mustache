8e6d6abf6377fd206a748b608484307112380ae0
Timm Friebe <thekid@thekid.de>
2013-12-10 12:27:11 +0100

    Fix "Class com\github\mustache\unittest\InMemory not found"

--- src/test/php/com/github/mustache/unittest/SpecificationTest.class.php
+++ src/test/php/com/github/mustache/unittest/SpecificationTest.class.php
@@ -1,6 +1,7 @@
 <?php namespace com\github\mustache\unittest;
 
 use com\github\mustache\MustacheEngine;
+use com\github\mustache\InMemory;
 use io\collections\iterate\ExtensionEqualsFilter;
 use io\collections\iterate\FilteredIOCollectionIterator;
 use io\collections\FileCollection;
@@ -11,17 +12,22 @@ use webservices\json\JsonFactory;
  * Executes the Mustache specifications
  *
  * To run, use:
- * <pre>
- *   $ wget 'https://github.com/mustache/spec/archive/master.zip' -O master.zip
- *   $ unzip master.zip && rm master.zip
- *   $ unittest com.github.mustache.SpecificationTest -a spec-master/specs
- * </pre>
+ * ```sh
+ * $ wget 'https://github.com/mustache/spec/archive/master.zip' -O master.zip
+ * $ unzip master.zip && rm master.zip
+ * $ unittest com.github.mustache.SpecificationTest -a spec-master/specs
+ * ```
  *
  * @see https://github.com/mustache/spec
  */
 class SpecificationTest extends \unittest\TestCase {
   protected $target= null;
 
+  /**
+   * Constructor
+   * @param string $name
+   * @param string $target The directory in which the spec files exist
+   */
   public function __construct($name, $target= null) {
     parent::__construct($name);
     $this->target= $target;
