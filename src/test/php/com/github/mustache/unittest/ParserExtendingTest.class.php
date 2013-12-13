b5a29c94284ffd2852bb0a6b3d0f309f83ebd0a5
Timm Friebe <thekid@thekid.de>
2013-12-13 13:36:07 +0100

    Test parser extension

---
+++ src/test/php/com/github/mustache/unittest/ParserExtendingTest.class.php
@@ -0,0 +1,21 @@
+<?php namespace com\github\mustache\unittest;
+
+use com\github\mustache\MustacheParser;
+use com\github\mustache\Node;
+use com\github\mustache\NodeList;
+
+class ParserExtendingTest extends \unittest\TestCase {
+
+  #[@test]
+  public function new_user_handler_as_function() {
+    $node= newinstance('com.github.mustache.Node', array(), '{
+      public function evaluate($context) { return "test"; }
+      public function __toString() { return "*test"; }
+    }');
+
+    $parser= create(new MustacheParser())->withHandler('*', true, function($tag, $state) use($node) {
+      $state->target->add($node);
+    });
+    $this->assertEquals(new NodeList(array($node)), $parser->parse('{{*test}}'));
+  }
+}
