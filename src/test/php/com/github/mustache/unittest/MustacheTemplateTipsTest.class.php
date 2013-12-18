2a78f4f617cba4695844cd1b914ae312b66793ae
Timm Friebe <thekid@thekid.de>
2013-12-18 20:49:33 +0100

    Add tip no. 3 from "Helpful Mustache Template Tips"

--- src/test/php/com/github/mustache/unittest/MustacheTemplateTipsTest.class.php
+++ src/test/php/com/github/mustache/unittest/MustacheTemplateTipsTest.class.php
@@ -19,20 +19,42 @@ class MustacheTemplateTipsTest extends \unittest\TestCase {
   }
 
   #[@test]
+  public function nr_3_default_values() {
+    $this->assertEquals(
+      "<h1>Title: Real Title</h1>\n".
+      "<h2>Sub-title: Default Sub-title</h2>\n".
+      "<h3>Third title: Default Third Title</h3>\n".
+      "<h4>Forth title: Default Fourth Title</h4>",
+      $this->render(
+        "<h1>Title: {{title}}{{^title}}Default Title{{/title}}</h1>\n".
+        "<h2>Sub-title: {{subtitle}}{{^subtitle}}Default Sub-title{{/subtitle}}</h2>\n".
+        "<h3>Third title: {{thirdTitle}}{{^thirdTitle}}Default Third Title{{/thirdTitle}}</h3>\n".
+        "<h4>Forth title: {{fourthTitle}}{{^fourthTitle}}Default Fourth Title{{/fourthTitle}}</h4>",
+        array(
+          'title'      => 'Real Title',
+          'subtitle'   => '',   // blank
+          'thirdTitle' => null,
+          // fourthTitle not defined
+        )
+      )
+    );
+  }
+
+  #[@test]
   public function nr_4_access_the_parent_context() {
     $this->assertEquals(
-        "<ul>\n".
-        "  <li>\n".
-        "    Title: Hat <br/>\n".
-        "    Color: black <br/>\n".
-        "    Author: Jones <br/>\n".
-        "  </li>\n".
-        "  <li>\n".
-        "    Title: Cat <br/>\n".
-        "    Color: red <br/>\n".
-        "    Author: Jones <br/>\n".
-        "  </li>\n".
-        "</ul>",
+      "<ul>\n".
+      "  <li>\n".
+      "    Title: Hat <br/>\n".
+      "    Color: black <br/>\n".
+      "    Author: Jones <br/>\n".
+      "  </li>\n".
+      "  <li>\n".
+      "    Title: Cat <br/>\n".
+      "    Color: red <br/>\n".
+      "    Author: Jones <br/>\n".
+      "  </li>\n".
+      "</ul>",
       $this->render(
         "<ul>\n".
         "  {{#slides}}\n".
