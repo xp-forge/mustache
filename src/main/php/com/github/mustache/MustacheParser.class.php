4926532fcaf036868242b4a02fe8f3b2b14b2238
Timm Friebe <thekid@thekid.de>
2014-01-02 20:35:50 +0100

    Refactor: Extract to base class

--- src/main/php/com/github/mustache/MustacheParser.class.php
+++ src/main/php/com/github/mustache/MustacheParser.class.php
@@ -5,32 +5,12 @@
  *
  * @test  xp://com.github.mustache.unittest.ParsingTest
  */
-class MustacheParser extends \lang\Object implements TemplateParser {
-  protected $handlers= array();
-  protected $standalone= array();
-  protected $options;
+class MustacheParser extends AbstractMustacheParser {
 
   /**
-   * Set up handlers
+   * Initialize this parser.
    */
-  public function __construct() {
-
-    // Tokenize name and options from a given tag, e.g.:
-    // * 'tag' = ['tag']
-    // * 'tag option "option 2"' = ['tag', 'option', 'option 2']
-    $this->options= function($tag) {
-      $parsed= array();
-      for ($o= 0, $l= strlen($tag); $o < $l; $o+= $p + 1) {
-        if ('"' === $tag{$o}) {
-          $p= strcspn($tag, '"', $o + 1) + 2;
-          $parsed[]= substr($tag, $o + 1, $p - 2);
-        } else {
-          $p= strcspn($tag, ' ', $o);
-          $parsed[]= substr($tag, $o, $p);
-        }
-      }
-      return $parsed;
-    };
+  protected function initialize() {
 
     // Sections
     $this->withHandler('#^', true, function($tag, $state, $options) {
@@ -95,101 +75,5 @@ class MustacheParser extends \lang\Object implements TemplateParser {
         : new VariableNode($parsed[0], true, array_slice($parsed, 1))
       );
     });
-  }
-
-  /**
-   * Add a handler
-   *
-   * @param  string $token Token characters to react on; use NULL to set the default handler
-   * @param  bool $standalone Whether these tags should be standalone on a line by itself
-   * @param  var $handler A function
-   * @return self
-   */
-  public function withHandler($tokens, $standalone, $handler) {
-    if (null === $tokens) {
-      $this->handlers[null]= $handler;
-    } else for ($i= 0; $i < strlen($tokens); $i++) {
-      $this->handlers[$tokens{$i}]= $handler;
-      $standalone && $this->standalone[$tokens{$i}]= true;
-    }
-    return $this;
-  }
-
-  /**
-   * Parse a template
-   *
-   * @param  string $template The template as a string
-   * @param  string $start Initial start tag, defaults to "{{"
-   * @param  string $end Initial end tag, defaults to "}}"
-   * @param  string $indent What to prefix before each line
-   * @return com.github.mustache.Node The parsed template
-   * @throws com.github.mustache.TemplateFormatException
-   */
-  public function parse($template, $start= '{{', $end= '}}', $indent= '') {
-    $state= new ParseState();
-    $state->target= new NodeList();
-    $state->start= $start;
-    $state->end= $end;
-    $state->parents= array();
-    $standalone= implode('', array_keys($this->standalone));
-
-    // Tokenize template
-    $lt= new \text\StringTokenizer($template, "\n", true);
-    while ($lt->hasMoreTokens()) {
-      $line= $indent.$lt->nextToken().$lt->nextToken();
-      $offset= 0;
-      do {
-
-        // Parse line
-        $state->padding= '';
-        if (false === ($s= strpos($line, $state->start, $offset))) {
-          $text= substr($line, $offset);
-          $tag= null;
-          $offset= strlen($line);
-        } else {
-          while (false === ($e= strpos($line, $state->end, $s+ strlen($state->start)))) {
-            if (!$lt->hasMoreTokens()) {
-              throw new TemplateFormatException('Unclosed '.$state->start.', expecting '.$state->end);
-            }
-            $line.= $indent.$lt->nextToken().$lt->nextToken();
-          }
-          $text= substr($line, $offset, $s- $offset);
-          $tag= substr($line, $s+ strlen($state->start), $e- $s- strlen($state->end));
-          $offset= $e + strlen($state->end);
-
-          // Check for standalone tags on a line by themselves
-          if (0 === strcspn($tag, $standalone)) {
-            if ('' === trim(substr($line, 0, $s).substr($line, $offset))) {
-              $offset= strlen($line);
-              $state->padding= substr($line, 0, $s);
-              $text= '';
-            }
-          }
-        }
-
-        // Handle text
-        if ('' !== $text) {
-          $state->target->add(new TextNode($text));
-        }
-
-        // Handle tag
-        if (null === $tag) {
-          continue;
-        } else if (isset($this->handlers[$tag{0}])) {
-          $f= $this->handlers[$tag{0}];
-        } else {
-          $f= $this->handlers[null];
-        }
-        $offset+= $f($tag, $state, $this->options);
-      } while ($offset < strlen($line));
-    }
-
-    // Check for unclosed sections
-    if (!empty($state->parents)) {
-      throw new TemplateFormatException('Unclosed section '.$state->target->name());
-    }
-
-    // \util\cmd\Console::writeLine($state->target);
-    return $state->target;
   }
 }
