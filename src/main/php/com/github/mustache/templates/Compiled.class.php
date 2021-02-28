63aa7e427c4dcfd66a3707ffb22332a5088ed7f7
Timm Friebe <thekid@thekid.de>
2021-02-28 10:54:32 +0100

    Relax type requirements for template parameter to Node

--- src/main/php/com/github/mustache/templates/Compiled.class.php
+++ src/main/php/com/github/mustache/templates/Compiled.class.php
@@ -3,12 +3,12 @@
 class Compiled extends Source {
   private $template;
 
-  /** @param com.github.mustache.Template */
+  /** @param com.github.mustache.Node */
   public function __construct($template) {
     $this->template= $template;
   }
 
-  /** @return com.github.mustache.Template */
+  /** @return com.github.mustache.Node */
   public function template() { return $this->template; }
 
   /** @return string */
@@ -21,7 +21,7 @@ class Compiled extends Source {
    * @param  string $start
    * @param  string $end
    * @param  string $indent
-   * @return com.github.mustache.Template
+   * @return com.github.mustache.Node
    */
   public function compile($parser, $start, $end, $indent) {
     return $this->template;
